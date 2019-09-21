ed687a33030aa097a938579ca43b1775e6e144a3
Guillaume CAMANES <guillaume.camanes@niji.fr>
2019-09-21 18:54:07 +0200

    # added store (actions, reducers, sagas), app config(constants, routes, config), login screen

---
+++ src/theme/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+  main: {
+    primary: '#ffffff',
+    secondary: '#30343F',
+    tertiary: '#7A7ABA',
+    quaternary: '#7FBFFF',
+  },
+  black: '#000000',
+  white: '#FFFFFF',
+  grey: '#E5E8EC',
+};
+
+export default {
+  palette: colors,
+};
