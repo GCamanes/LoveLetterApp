b75c8de6c9f88001c82278880f0140ec44079005
Guillaume CAMANES <guillaume.camanes@niji.fr>
2019-09-22 21:14:30 +0200

    # reworked adding player view style

--- src/components/AddingPlayerView.js
+++ src/components/AddingPlayerView.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import {
+  ActivityIndicator,
   StyleSheet,
   Text,
   TextInput,
@@ -10,7 +11,7 @@ import {
 import {connect} from 'react-redux';
 
 import * as PlayerActions from '../redux/actions/player-actions';
-import {AppColors, AppFonts, AppSizes} from '../theme';
+import { AppColors, AppFonts, AppSizes, AppStyles } from '../theme';
 import showAlert from '../utils/showAlert';
 import AppConstants from '../app/app.constants';
 
@@ -29,19 +30,20 @@ const styles = StyleSheet.create({
   },
   addingPlayerView: {
     alignItems: 'center',
+    justifyContent: 'center',
+    width: AppSizes.screen.width * 0.8,
+    height: AppSizes.screen.widthThird,
     backgroundColor: AppColors.palette.main.tertiary,
     borderRadius: 20,
     borderWidth: 2,
     borderColor: AppColors.palette.main.secondary,
-    paddingHorizontal: 30,
-    paddingVertical: 20,
   },
   input: {
     width: AppSizes.screen.width * 0.65,
     height: 40,
-    marginBottom: 15,
     paddingVertical: 0,
     paddingHorizontal: 10,
+    marginBottom: 10,
     color: AppColors.palette.main.tertiary,
     backgroundColor: AppColors.palette.main.primary,
     borderColor: AppColors.palette.main.secondary,
@@ -49,6 +51,11 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     fontSize: AppFonts.t16.size,
     textAlignVertical: 'center',
+  },
+  buttonView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: AppSizes.screen.width * 0.65,
   },
   touchableView: {
     height: 40,
@@ -59,7 +66,6 @@ const styles = StyleSheet.create({
     backgroundColor: AppColors.palette.main.quaternary,
     alignItems: 'center',
     justifyContent: 'center',
-    marginHorizontal: 5,
   },
   text: {
     color: AppColors.palette.main.primary,
@@ -77,18 +83,22 @@ class AddingPlayerView extends Component {
   }
 
   onAddPress = () => {
-    const {addPlayer} = this.props;
+    const {addPlayer, connectivity} = this.props;
     const {name} = this.state;
-    if (name === '') {
-      showAlert('Please enter a player name');
-    } else if (name.length > AppConstants.GAME.MAX_LENGTH_PLAYER_NAME) {
-      showAlert(
-        `max name length ${AppConstants.GAME.MAX_LENGTH_PLAYER_NAME} (current ${
-          name.length
-        })`,
-      );
+    if (connectivity) {
+      if (name === '') {
+        showAlert('Please enter a player name');
+      } else if (name.length > AppConstants.GAME.MAX_LENGTH_PLAYER_NAME) {
+        showAlert(
+          `max name length ${
+            AppConstants.GAME.MAX_LENGTH_PLAYER_NAME
+          } (current ${name.length})`,
+        );
+      } else {
+        addPlayer(name);
+      }
     } else {
-      addPlayer(name);
+      showAlert('No internet connection');
     }
   }
 
@@ -101,27 +111,38 @@ class AddingPlayerView extends Component {
    * Render function to display component.
    */
   render() {
+    const {loadingStatus} = this.props;
     const {name} = this.state;
     return (
       <View style={styles.main_container}>
-        <View style={styles.addingPlayerView}>
-          <TextInput
-            value={this.state.name}
-            onChangeText={name => this.setState({name})}
-            placeholder="Name"
-            selectionColor={AppColors.palette.main.secondary}
-            style={styles.input}
-            autoCapitalize="none"
-          />
-          <View style={{flexDirection: 'row'}}>
-            <TouchableOpacity style={styles.touchableView} onPress={this.onAddPress}>
-              <Text style={styles.text}>Add</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.touchableView} onPress={this.onCancelPress}>
-              <Text style={styles.text}>Cancel</Text>
-            </TouchableOpacity>
+        {loadingStatus.loading && (
+          <View style={styles.addingPlayerView}>
+            <ActivityIndicator
+              size="large"
+              color={AppColors.palette.main.quaternary}
+            />
           </View>
-        </View>
+        )}
+        {!loadingStatus.loading && (
+          <View style={styles.addingPlayerView}>
+            <TextInput
+              value={name}
+              onChangeText={name => this.setState({name})}
+              placeholder="Name"
+              selectionColor={AppColors.palette.main.secondary}
+              style={styles.input}
+              autoCapitalize="none"
+            />
+            <View style={styles.buttonView}>
+              <TouchableOpacity style={styles.touchableView} onPress={this.onAddPress}>
+                <Text style={styles.text}>Add</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.touchableView} onPress={this.onCancelPress}>
+                <Text style={styles.text}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
       </View>
     );
   }
@@ -129,10 +150,22 @@ class AddingPlayerView extends Component {
 
 AddingPlayerView.propTypes = {
   addPlayer: PropTypes.func.isRequired,
+  connectivity: PropTypes.bool.isRequired,
+  loadingStatus: PropTypes.object,
   showAddingPlayerView: PropTypes.func.isRequired,
 };
 
+AddingPlayerView.defaultProps = {
+  loadingStatus: {loading: false},
+};
+
+// What data from the store shall we send to the component?
+const mapStateToProps = state => ({
+  connectivity: state.app.connectivity,
+  loadingStatus: state.app[AppConstants.ROUTES.ADD_PLAYER],
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   PlayerActions,
 )(AddingPlayerView);
