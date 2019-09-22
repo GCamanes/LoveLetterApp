d331a4c3bdef42aedaf25ce1c885863764afe36b
Guillaume CAMANES <guillaume.camanes@niji.fr>
2019-09-22 23:17:56 +0200

    # added cards and decks constants

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -1,4 +1,155 @@
 const AppConstants = {
+  CARD: {
+    ASSASSIN: 'assassin',
+    BARON: 'baron',
+    BARONNE: 'baronne',
+    COMPTESSE: 'comptesse',
+    DOUBLE_GARDES: 'doubleGardes',
+    ESCROC: 'escroc',
+    FAUSSAIRE: 'faussaire',
+    GARDE: 'garde',
+    GENERAL: 'general',
+    INTENDANT: 'intendant',
+    PRETRE: 'pretre',
+    PRINCE: 'prince',
+    PRINCESSE: 'princesse',
+    ROI: 'roi',
+    SERVANTE: 'servante',
+    TUEUR_A_GAGE: 'tueurAGage',
+    VOYANTE: 'voyante',
+  },
+  DECKS: {
+    4: {
+      // 0
+      ASSASSIN: 0,
+      TUEUR_A_GAGE: 0,
+      // 1
+      GARDE: 5,
+      FAUSSAIRE: 0,
+      DOUBLE_GARDES: 0,
+      GENERAL: 0,
+      // 2
+      PRETRE: 2,
+      ESCROC: 0,
+      // 3
+      BARON: 2,
+      BARONNE: 0,
+      // 4
+      SERVANTE: 2,
+      INTENDANT: 0,
+      //5
+      PRINCE: 2,
+      VOYANTE: 0,
+      // 6+
+      ROI: 1,
+      COMPTESSE: 1,
+      PRINCESSE: 1,
+    },
+    5: {
+      // 0
+      ASSASSIN: 1,
+      TUEUR_A_GAGE: 0,
+      // 1
+      GARDE: 6,
+      FAUSSAIRE: 0,
+      DOUBLE_GARDES: 0,
+      GENERAL: 0,
+      // 2
+      PRETRE: 2,
+      ESCROC: 1,
+      // 3
+      BARON: 2,
+      BARONNE: 1,
+      // 4
+      SERVANTE: 2,
+      INTENDANT: 0,
+      //5
+      PRINCE: 2,
+      VOYANTE: 0,
+      // 6+
+      ROI: 1,
+      COMPTESSE: 1,
+      PRINCESSE: 1,
+    },
+    6: {
+      // 0
+      ASSASSIN: 2,
+      TUEUR_A_GAGE: 0,
+      // 1
+      GARDE: 6,
+      FAUSSAIRE: 1,
+      DOUBLE_GARDES: 0,
+      GENERAL: 0,
+      // 2
+      PRETRE: 3,
+      ESCROC: 1,
+      // 3
+      BARON: 2,
+      BARONNE: 1,
+      // 4
+      SERVANTE: 2,
+      INTENDANT: 1,
+      //5
+      PRINCE: 2,
+      VOYANTE: 0,
+      // 6+
+      ROI: 1,
+      COMPTESSE: 1,
+      PRINCESSE: 1,
+    },
+    7: {
+      // 0
+      ASSASSIN: 2,
+      TUEUR_A_GAGE: 0,
+      // 1
+      GARDE: 6,
+      FAUSSAIRE: 2,
+      DOUBLE_GARDES: 0,
+      GENERAL: 0,
+      // 2
+      PRETRE: 4,
+      ESCROC: 2,
+      // 3
+      BARON: 2,
+      BARONNE: 1,
+      // 4
+      SERVANTE: 2,
+      INTENDANT: 1,
+      //5
+      PRINCE: 2,
+      VOYANTE: 1,
+      // 6+
+      ROI: 1,
+      COMPTESSE: 1,
+      PRINCESSE: 1,
+    },
+    8: {
+      // 0
+      ASSASSIN: 2,
+      TUEUR_A_GAGE: 1,
+      // 1
+      GARDE: 6,
+      FAUSSAIRE: 2,
+      DOUBLE_GARDES: 1,
+      GENERAL: 1,
+      // 2
+      PRETRE: 4,
+      ESCROC: 2,
+      // 3
+      BARON: 3,
+      BARONNE: 3,
+      // 4
+      SERVANTE: 2,
+      INTENDANT: 1,
+      //5
+      PRINCE: 2,
+      VOYANTE: 1,
+      // 6+
+      ROI: 1,
+      COMPTESSE: 1,
+      PRINCESSE: 1,
+    },
+  },
   EVENTS: {
     // login
     INIT_LOGIN_PAGE_SAGA: 'INIT_LOGIN_PAGE_SAGA',
