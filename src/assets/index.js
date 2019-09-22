2cfd8370ef062e01e4e9b12119271e20c829006b
Guillaume CAMANES <guillaume.camanes@niji.fr>
2019-09-22 22:49:17 +0200

    # imported all card image files

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,25 @@ const assets = {
   play: require('./play-icon.png'),
   plus: require('./plus-icon.png'),
   trophy: require('./trophy-icon.png'),
+  card: {
+    assassin: require('./cards/card-assassin.png'),
+    baron: require('./cards/card-baron.png'),
+    baronne: require('./cards/card-baronne.png'),
+    comptesse: require('./cards/card-comptesse.png'),
+    doubleGardes: require('./cards/card-double-gardes.png'),
+    escroc: require('./cards/card-escroc.png'),
+    faussaire: require('./cards/card-faussaire.png'),
+    garde: require('./cards/card-garde.png'),
+    general: require('./cards/card-general.png'),
+    intendant: require('./cards/card-intendant.png'),
+    pretre: require('./cards/card-pretre.png'),
+    prince: require('./cards/card-prince.png'),
+    princesse: require('./cards/card-princesse.png'),
+    roi: require('./cards/card-roi.png'),
+    servante: require('./cards/card-servante.png'),
+    tueurAGage: require('./cards/card-tueur-a-gage.png'),
+    voyante: require('./cards/card-voyante.png'),
+  },
 };
 
 export default assets;
